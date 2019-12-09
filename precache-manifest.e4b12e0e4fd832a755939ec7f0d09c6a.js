bbe1e267e5bde61e9c74f2abcf7dbe29c95cbed0
riya242k <guptariyapriya99@gmail.com>
2019-12-09 15:37:08 +0530

    Updates

---
+++ precache-manifest.e4b12e0e4fd832a755939ec7f0d09c6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31ad88300a3a8e87eb16c7685aa025a1",
+    "url": "/phone-directory/index.html"
+  },
+  {
+    "revision": "d83175af546300dd5069",
+    "url": "/phone-directory/static/css/main.effe12bb.chunk.css"
+  },
+  {
+    "revision": "a3ddb1b2fc089ecbeb84",
+    "url": "/phone-directory/static/js/2.1c934388.chunk.js"
+  },
+  {
+    "revision": "7fc60fe721fa046663bd312b1d917655",
+    "url": "/phone-directory/static/js/2.1c934388.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d83175af546300dd5069",
+    "url": "/phone-directory/static/js/main.9df2f9c4.chunk.js"
+  },
+  {
+    "revision": "505cdc9333590f35c409",
+    "url": "/phone-directory/static/js/runtime-main.53d98e78.js"
+  }
+]);
